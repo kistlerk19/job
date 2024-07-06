49f360e21605bf3e3ec605e7ac6a3e2797b5c347
Ishmael Gyamfi <isgyamfi25@gmail.com>
2024-07-06 23:31:13 +0000

    first commit

---
+++ src/main/java/com/mere/travail/TravailApplication.java
@@ -0,0 +1,13 @@
+package com.mere.travail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravailApplication.class, args);
+	}
+
+}
